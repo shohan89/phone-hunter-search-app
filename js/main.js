4c0602594130b33016d89c9551309bffc8d099f7
shohan89 <shohansharoar89@gmail.com>
2022-08-30 09:06:43 +0600

    new feature - show all search result when button clicked

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
-const loadData = searchText => {
+const loadData = (searchText, dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   fetch(url)
   .then(res => res.json())
-  .then(data => displayData(data.data));
+  .then(data => displayData(data.data, dataLimit));
 }
 
-const displayData = phones => {
+const displayData = (phones, dataLimit) => {
   // console.log(phones);
   const phoneContainer = document.getElementById('phones-container');
   phoneContainer.innerText = '';
+  //display 10 search result only
+  const showAll = document.getElementById('show-all');
+  if (dataLimit && phones.length > 10) {
+    phones = phones.slice(0, 10);
+    showAll.classList.remove('d-none');
+  }
+  else {
+    showAll.classList.add('d-none');
+  }
+  
 
   // Display Not Found Message If Search Result is Empty
   const notFound = document.getElementById('not-found-message');
@@ -40,15 +50,21 @@ const displayData = phones => {
 
 }
 
+//process search
+const processSearch = (dataLimit) => {
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  loadData(searchText, dataLimit);
+}
 
 // Search Button functionality
 document.getElementById('search-btn').addEventListener('click', () => {
   // start spinner
   toggleSpinner(true);
 
-  const searchField = document.getElementById('search-field');
-  const searchText = searchField.value;
-  loadData(searchText);
+  processSearch(10);
+
+  
 })
 
 
@@ -63,6 +79,13 @@ const toggleSpinner = isLoading => {
   }
 }
 
+
+// load show all data when button clicked
+document.getElementById('btn-show-all').addEventListener('click', () => {
+
+  processSearch();
+
+})
 
 
 // loadData();
